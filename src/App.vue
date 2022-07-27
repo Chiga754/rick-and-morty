f2ea4591f63b0ff21c9befa6d0eef50b8a4aa49c
Chiga754 <a.chigarev96@gmail.com>
2022-07-27 07:30:48 +0500

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  
+  <header-section />
+  <character-list />
 </template>
 
+<script>
+import CharacterList from './components/CharacterList.vue'
+import HeaderSection from './components/HeaderSection.vue'
+export default {
+  components: {
+    HeaderSection,
+    CharacterList,
+  }
+}
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  background: #999999;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
 }
 </style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="header">
+        <h1>Сharacters from Rick and Morty</h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header{
+    background: #000000;
+    color: #ffffff;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    background-image: url(@/assets/bg-header.png);
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: right;
+}
+h1{
+    font-size: 50px;
+}
+</style>
